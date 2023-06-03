75e2331cbb118e28c5ae8136d3d2cd658767e648
gikandiDan <gikandin@gmail.com>
2023-06-03 11:58:57 +0300

    Initial Commit

---
+++ conditionals.py
@@ -0,0 +1,7 @@
+age = int(input('Enter your age in years: '))
+if age<18:
+   print("You are not eligible to vote")
+   print("Grow up!")
+else:
+   print("Bingo!!")
+   print("Head to the polling station!")
